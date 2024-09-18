85836653702c7da5d726dec86698862dfb54b0ad
alexey <i.leha98@mail.ru>
2024-09-18 10:56:03 +0400

    +RazorPages

--- Program.cs
+++ Program.cs
@@ -5,58 +5,73 @@ namespace AspNetChat
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+			var builder = WebApplication.CreateBuilder(args);
 
-            //app.MapGet("/", () => "Hello World!");
-            //app.UseWelcomePage();
-            //app.Run(async (context) => await context.Response.WriteAsync("Hello METANIT.COM"));
+			// Add services to the container.
+			builder.Services.AddRazorPages();
 
-            app.UseMiddleware<TokenMiddleware>("123");
+			var app = builder.Build();
 
-            app.UseDefaultFiles();  // поддержка страниц html по умолчанию
-            app.UseStaticFiles();   // добавляем поддержку статических файлов
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+            #region test
 
             app.UseWhen(
-            context => context.Request.Path == "/time", // если путь запроса "/time"
-            appBuilder =>
-            {
-                // логгируем данные - выводим на консоль приложения
-                appBuilder.Use(async (context, next) =>
-                {
-                    var time = DateTime.Now.ToShortTimeString();
-                    Console.WriteLine($"Time: {time}");
-                    await next();   // вызываем следующий middleware
-                });
+			context => context.Request.Path == "/time", // если путь запроса "/time"
+			appBuilder =>
+			{
+				// логгируем данные - выводим на консоль приложения
+				appBuilder.Use(async (context, next) =>
+				{
+					var time = DateTime.Now.ToShortTimeString();
+					Console.WriteLine($"Time: {time}");
+					await next();   // вызываем следующий middleware
+				});
 
-                // отправляем ответ
-                appBuilder.Run(async context =>
-                {
-                    var time = DateTime.Now.ToShortTimeString();
-                    await context.Response.WriteAsync($"Time: {time}");
-                });
-            });
+				// отправляем ответ
+				appBuilder.Run(async context =>
+				{
+					var time = DateTime.Now.ToShortTimeString();
+					await context.Response.WriteAsync($"Time: {time}");
+				});
+			});
 
-            app.MapGet("/routes",
-                (IEnumerable<EndpointDataSource> endpointSources) => string.Join("\n", endpointSources.SelectMany(source => source.Endpoints)));
+			app.MapGet("/routes",
+				(IEnumerable<EndpointDataSource> endpointSources) => string.Join("\n", endpointSources.SelectMany(source => source.Endpoints)));
 
-            app.Map("/users/{id:int:range(0, 1000)}", (int id) => $"User Id: {id}");
+			app.Map("/users/{id:int:range(0, 1000)}", (int id) => $"User Id: {id}");
 
-            app.Map("/users/params", (int id) => $"User params Id: {id}");
+			app.Map("/users/params", (int id) => $"User params Id: {id}");
 
-            app.Map(
-                "map2/{controller=Home}/{action=Index}/{id?}",
-                (string controller, string action, string? id) =>
-                    $"Controller: {controller} \nAction: {action} \nId: {id}"
-            );
+			app.Map(
+				"map2/{controller=Home}/{action=Index}/{id?}",
+				(string controller, string action, string? id) =>
+					$"Controller: {controller} \nAction: {action} \nId: {id}"
+			);
 
-            app.Map("map3/{**info}", (string? info) => $"map3 info: {info ?? "n/a"}");
+			app.Map("map3/{**info}", (string? info) => $"map3 info: {info ?? "n/a"}");
 
-            app.Map("map4/{*info}", (string? info) => $"map4 info: {info ?? "n/a"}");
+			app.Map("map4/{*info}", (string? info) => $"map4 info: {info ?? "n/a"}");
 
-            //app.Map("/", () => "Hello METANIT.COM");
+            #endregion
 
-            app.Run();
-        }
+
+            app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.MapRazorPages();
+
+			app.Run();
+		}
     }
 }
